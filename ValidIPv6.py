9e2dbea793342a7700a59a534a4f657014982f6e
taylor-red-1989 <67401259+taylor-red-1989@users.noreply.github.com>
2021-06-26 08:02:38 +0530

    Add files via upload

---
+++ ValidIPv6.py
@@ -0,0 +1,16 @@
+def valid(x):
+    v = 'ABCDEFabcdef0123456789'
+    n = x.split(':')
+    if len(n)>8:
+        return False
+    for i in n:
+        if len(i)>4:
+            return False
+        for j in i:
+            if j not in v:
+                return False
+            else:
+                continue
+    return True
+
+print(valid('3FFE::1:200:F8FF:FE75:50DF'))
